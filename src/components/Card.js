55fcfa494651368b9302b38b73797a2814ae0917
Allen <Allenlea2007@gmail.com>
2021-01-20 15:03:14 -0600

    made card container for app, made card component. made method to fetch card state from backend and display them using cards on front end.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,14 @@
-export function Card(){
+export function Card({props}){
+    let {title, image,caption} = props
     return(
         <div className="card">
-            This is a card
-            <img src="https://lh3.googleusercontent.com/proxy/L6Ta0oKDq6gJAwKIudC3_sr1VSXi_ga9zJ7NRML8-rgJsBjC8InQcopfHz29eNTF9hgj10gC6E2daSjOQNNejK4wrviRInA3bWPghywZjdur1z5yhEzAIvu4aQ1mtdYOT-62pnYd4H20_Iwhh_S4bpaGg2J9NxrMH-uHLEXyX7KtBH9uQ1oLAcwQxQLrWc4MhN99KE7ZGV0YG2s2XXyRAmD12PysUgAgGew0Kf0W9nXxEtzsXA" alt="moving in history"/>
+            <p className="title">
+                {title}
+            </p>
+            <img src={image} alt="moving in history"/>
+            <p className="caption">
+                {caption}
+            </p>
         </div>
     )
 }
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,9 +1,36 @@
+import { Component } from 'react'
 import { Card } from './Card'
 
-export function CardContainer(){
-    return(
-        <div className="card-container">
-            <Card/>
-        </div>
-    )
+export class CardContainer extends Component {
+
+    state = {
+        cards:[]
+    }
+
+    fetchCards = () => {
+        fetch('http://localhost:3000/displays')
+            .then(response => response.json())
+            .then(response => {
+                this.setState({cards: response})
+            })
+    }
+
+    componentDidMount = () =>{
+        this.fetchCards()
+    }
+
+    displayCard = () =>  this.state.cards.map(card => (
+            <Card props={card}/>
+            ))
+    
+    
+        render(){
+        return(
+            <div className="card-container">
+                {this.displayCard()}
+            </div>
+        )
+    }
 }
+
+// git@github.com:lukethinnes/Hackathon.git
