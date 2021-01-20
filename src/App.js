dffab6264eaf6da17c80e8f7c8b4c82bdbeed2c3
Allen <Allenlea2007@gmail.com>
2021-01-20 12:07:20 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { CardContainer } from './components/CardContainer'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CardContainer/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/CardContainer.js
@@ -0,0 +1,9 @@
+import { Card } from './Card'
+
+export function CardContainer(){
+    return(
+        <div className="card-container">
+            <Card/>
+        </div>
+    )
+}
